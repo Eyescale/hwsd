e5be0800ff3f6cbf5f84c2c745adbb3755567cce
Stefan Eilemann <eilemann@gmail.com>
2012-05-25 09:30:02 +0200

    Darwin build fix

--- libs/gpusd/dns_sd/module.cpp
+++ libs/gpusd/dns_sd/module.cpp
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2011, Stefan Eilemann <eile@eyescale.ch> 
+/* Copyright (c) 2011-2012, Stefan Eilemann <eile@eyescale.ch> 
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -20,6 +20,7 @@
 #include <gpusd/gpuInfo.h>
 #include <servus/servus.h>
 #include <algorithm>
+#include <sstream>
 
 #define WAIT_TIME 500 // ms
 
