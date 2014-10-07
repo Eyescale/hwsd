3bd4b5b2e5f0bd7df2b9e152531788258621246d
Ahmet Bilgili <ahmet.bilgili@epfl.ch>
2014-10-07 12:10:47 +0200

    Updated the CMake files

--- CMake/hwsd.cmake
+++ CMake/hwsd.cmake
@@ -7,7 +7,8 @@ set(HWSD_DEPENDS OpenGL Qt4 X11 REQUIRED Lunchbox Boost)
 set(HWSD_BOOST_COMPONENTS "program_options regex system")
 set(HWSD_QT4_COMPONENTS "QtNetwork")
 set(HWSD_DEB_DEPENDS libx11-dev libgl1-mesa-dev libqt4-dev
-  libboost-program-options-dev libboost-regex-dev libboost-system-dev)
+  libboost-program-options-dev libboost-regex-dev 
+  libboost-system-dev)
 set(HWSD_PORT_DEPENDS boost qt4-mac)
 set(HWSD_FORCE_BUILD ${CI_BUILD})
 
