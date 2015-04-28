5dd549703b9c3f37293ad429485c83b824919b89
Daniel Nachbaur <danielnachbaur@gmail.com>
2015-04-28 14:39:25 +0200

    Port to Qt5

--- CMake/hwsd.cmake
+++ CMake/hwsd.cmake
@@ -3,13 +3,12 @@
 set(HWSD_PACKAGE_VERSION 1.1)
 set(HWSD_REPO_URL https://github.com/Eyescale/hwsd.git)
 set(HWSD_REPO_TAG master)
-set(HWSD_DEPENDS OpenGL Qt4 X11 REQUIRED Lunchbox Boost)
+set(HWSD_DEPENDS OpenGL Qt5Core Qt5Network X11 REQUIRED Lunchbox Boost)
 set(HWSD_BOOST_COMPONENTS "program_options regex system")
-set(HWSD_QT4_COMPONENTS "QtNetwork")
-set(HWSD_DEB_DEPENDS libx11-dev libgl1-mesa-dev libqt4-dev
-  libboost-program-options-dev libboost-regex-dev 
+set(HWSD_DEB_DEPENDS libx11-dev libgl1-mesa-dev qtbase5-dev
+  libboost-program-options-dev libboost-regex-dev
   libboost-system-dev)
-set(HWSD_PORT_DEPENDS boost qt4-mac)
+set(HWSD_PORT_DEPENDS boost qt5-mac)
 set(HWSD_FORCE_BUILD ${CI_BUILD})
 
 if(CI_BUILD_COMMIT)
